274b17025bde0f1a5332a37394a3dc8293e41d2d
kaz-taka-1 <k.t.niconico.physics@gmail.com>
2021-07-19 10:19:01 +0900

    メニューの表示

--- app/src/main/java/com/websarva/wings/android/menusample/MainActivity.kt
+++ app/src/main/java/com/websarva/wings/android/menusample/MainActivity.kt
@@ -8,4 +8,37 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    private fun createTeishokuLiost():MutableList<MutableMap<String,Any>>{
+        val menuList:MutableList<MutableMap<String,Any>> = mutableListOf()
+        var menu = mutableMapOf<String,Any>("name" to "から揚げ定食","price" to 800,"desc" to "若鶏のから揚げにサラダ、ご飯と味噌汁がつきます。")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "ハンバーグ定食", "price" to 850, "desc" to "手ごねハンバーグにサラダ、ご飯とお味噌汁が付きます。")
+        menuList.add(menu)
+        // 以下データ登録の繰り返し。
+        menu = mutableMapOf("name" to "生姜焼き定食", "price" to 850, "desc" to "すりおろし生姜を使った生姜焼きにサラダ、ご飯とお味噌汁が付きます。")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "ステーキ定食", "price" to 1000, "desc" to "国産牛のステーキにサラダ、ご飯とお味噌汁が付きます。")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "野菜炒め定食", "price" to 750, "desc" to "季節の野菜炒めにサラダ、ご飯とお味噌汁が付きます。")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "とんかつ定食", "price" to 900, "desc" to "ロースとんかつにサラダ、ご飯とお味噌汁が付きます。")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "ミンチかつ定食", "price" to 850, "desc" to "手ごねミンチカツにサラダ、ご飯とお味噌汁が付きます。")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "チキンカツ定食", "price" to 900, "desc" to "ボリュームたっぷりチキンカツにサラダ、ご飯とお味噌汁が付きます。")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "コロッケ定食", "price" to 850, "desc" to "北海道ポテトコロッケにサラダ、ご飯とお味噌汁が付きます。")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "回鍋肉定食", "price" to 750, "desc" to "ピリ辛回鍋肉にサラダ、ご飯とお味噌汁が付きます。")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "麻婆豆腐定食", "price" to 800, "desc" to "本格四川風麻婆豆腐にサラダ、ご飯とお味噌汁が付きます。")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "青椒肉絲定食", "price" to 900, "desc" to "ピーマンの香り豊かな青椒肉絲にサラダ、ご飯とお味噌汁が付きます。")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "焼き魚定食", "price" to 850, "desc" to "鰆の塩焼きにサラダ、ご飯とお味噌汁が付きます。")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "焼肉定食", "price" to 950, "desc" to "特性たれの焼肉にサラダ、ご飯とお味噌汁が付きます。")
+        menuList.add(menu)
+        return menuList
+    }
 }
